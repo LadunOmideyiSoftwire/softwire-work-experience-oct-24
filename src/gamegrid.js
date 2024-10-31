d7282a6cf54203a2cfd23fe2586dca2ba895910b
Matthew-Priest <mwpriest08@gmail.com>
2024-10-31 10:45:17 +0000

    Update src/gamegrid.js
    Co-authored-by: Samuel Young <samuel.young@softwire.com>

--- src/gamegrid.js
+++ src/gamegrid.js
@@ -1,6 +1,6 @@
-let rows = 20;
-let cols = 10;
-let gamegrid = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
+const rows = 20;
+const cols = 10;
+const gamegrid = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
 for (let y = 0; y < 20; y++) {
     for (let x = 0; x < 10; x++) {       
         console.log(gamegrid[y][x]);
