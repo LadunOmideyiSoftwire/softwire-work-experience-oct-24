bc10ebf50684b87320d1a5ff2d56349bb788e209
Ladun Omideyi <ladun.omideyi@softwire.com>
2024-11-01 11:26:48 +0000

    clean up commit

--- src/index.js
+++ src/index.js
@@ -55,13 +55,51 @@ if (canplacepiece(startcol, startrow, Randompiece)) { // Check if piece can be p
     console.log("Cannot place");
 }
 
-function rendergrid(gamegrid) { // renders the gamegrid
-    for (let row of gamegrid) {
-        console.log(row.join(' '));
+// function rendergrid(gamegrid) { // renders the gamegrid
+//     for (let row of gamegrid) {
+//         console.log(row.join(' '));
+//     }
+// }
+
+rendergrid(gamegrid); // Render the grid after placing a piece
+
+function renderGamegrid(gamegrid, rows, cols){
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.lineWidth = 5;
+    ctx.fillStyle = 'orange'; // Set your desired background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill the entire canvas
+
+    const cellHeight = 100;
+    const cellWidth = 100;
+    const padding = 5;
+    let xcoordinate = 0;
+    let ycoordinate = 0;
+    let cell_fill_colour = "yellow";
+    let rows = 20;
+    let cols = 10;
+
+    for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 10; j++) {
+        ctx.moveTo(xcoordinate,ycoordinate); 
+
+        if (gamegrid[rows + i][cols + j] !==0){ 
+            ctx.fillStyle = gamegrid[rows + i][cols + j]
+        // if (xcoordinate > 200) { ctx.fillStyle = "green"
+             } 
+            else
+            {ctx.fillStyle = cell_fill_colour;}
+        ctx.fillRect(xcoordinate,ycoordinate, cellWidth,cellHeight);  
+        xcoordinate += cellWidth;
+        console.log(xcoordinate,ycoordinate, cellWidth,cellHeight)
+        }
+    xcoordinate = 0
+    ycoordinate += cellHeight;
+    console.log(xcoordinate,ycoordinate);
     }
 }
 
-rendergrid(gamegrid); // Render the grid after placing a piece
+
 
 let piecePosition = {x: 4, y: 0}
 function MovePiece(direction){ // function that moves the piece
@@ -97,58 +135,23 @@ function collision(){ // checks if pieces are touching and stops them from colli
 }
 
 
-document.addEventListener('keydown', (event) => { // calls the function to execuet when the corresponding arrow key is pressed
-    if(event.key === 'ArrowLeft'){
-        MovePiece('left');
-        if (collision()){
-            piecePosition.x -= 1;
-        }
-    } else if (event.key === 'ArrowRight'){
-        MovePiece('right');
-        if (collision()){
-            piecePosition.x += 1;
-        }
-    }else if (event.key === 'ArrowDown'){
-        MovePiece('down');
-        if (collision()){
-            piecePosition.y -= 1;
-        }
-    }
-})
+// document.addEventListener('keydown', (event) => { // calls the function to execuet when the corresponding arrow key is pressed
+//     if(event.key === 'ArrowLeft'){
+//         MovePiece('left');
+//         if (collision()){
+//             piecePosition.x -= 1;
+//         }
+//     } else if (event.key === 'ArrowRight'){
+//         MovePiece('right');
+//         if (collision()){
+//             piecePosition.x += 1;
+//         }
+//     }else if (event.key === 'ArrowDown'){
+//         MovePiece('down');
+//         if (collision()){
+//             piecePosition.y -= 1;
+//         }
+//     }
+// })
 
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-ctx.lineWidth = 5;
-ctx.fillStyle = 'orange'; // Set your desired background color
-ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill the entire canvas
-
-const cellHeight = 100;
-const cellWidth = 100;
-const padding = 5;
-let xcoordinate = 0;
-let ycoordinate = 0;
-let cell_fill_colour = "yellow";
-let rows = 20;
-let cols = 10;
-
-function renderGamegrid(gamegrid, rows, cols){
-    for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 10; j++) {
-        ctx.moveTo(xcoordinate,ycoordinate); 
-
-        // if (gamegrid[rows + i][cols + j] !==0){ 
-        //     ctx.fillStyle = gamegrid[rows + i][cols + j]
-        if (xcoordinate > 200) { ctx.fillStyle = "green"
-             } 
-            else
-            {ctx.fillStyle = cell_fill_colour;}
-        ctx.fillRect(xcoordinate,ycoordinate, cellWidth,cellHeight);  
-        xcoordinate += cellWidth;
-        console.log(xcoordinate,ycoordinate, cellWidth,cellHeight)
-        }
-    xcoordinate = 0
-    ycoordinate += cellHeight;
-    console.log(xcoordinate,ycoordinate);
-    }
-}
 
